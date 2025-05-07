7e20961e405dc134c88e179c403938f676ae7597
shakoabramidhvili <shalva.abramishvili@gmail.com>
2025-05-07 18:07:23 +0400

    fix port

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.use(graphqlUploadExpress({ maxFileSize: 5000000, maxFiles: 10 }));
   app.useGlobalPipes(new ValidationPipe());
   
-  await app.listen(9000);
+  await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
