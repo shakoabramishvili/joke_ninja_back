53bd34f768b8af71ad629a6a085a4399fca12ea5
Levan <zedelashvililevani@gmail.com>
2025-05-24 20:46:14 +0400

    we were return user without modify it

--- src/app/follower/follower.service.ts
+++ src/app/follower/follower.service.ts
@@ -3,8 +3,10 @@ import { Follower, FollowerDocument } from './entities/follower.entity';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, Schema as MongooSchema } from 'mongoose';
 import { User, UserDocument } from '../user/entities/user.entity';
-import { NotificationService, sendNotification } from '../notification/notification.service';
-
+import {
+  NotificationService,
+  sendNotification,
+} from '../notification/notification.service';
 
 @Injectable()
 export class FolloweService {
@@ -13,10 +15,13 @@ export class FolloweService {
     private followerModel: Model<FollowerDocument>,
     @InjectModel(User.name)
     private userModel: Model<UserDocument>,
-    private readonly notificationService: NotificationService
+    private readonly notificationService: NotificationService,
   ) {}
 
-  async createFollower(followerId: MongooSchema.Types.ObjectId, followingId: MongooSchema.Types.ObjectId ) {
+  async createFollower(
+    followerId: MongooSchema.Types.ObjectId,
+    followingId: MongooSchema.Types.ObjectId,
+  ) {
     // Prevent self-following (optional)
     if (followerId === followingId) {
       throw new Error("you_can't_follow_yourself");
@@ -34,25 +39,33 @@ export class FolloweService {
     // Create new follow
     const createdFollower = new this.followerModel({
       following: followingId,
-      follower: followerId
+      follower: followerId,
     });
     await createdFollower.save();
 
     // Update follower/following counts
-    const follower = await this.userModel.findByIdAndUpdate(followerId, { $inc: {followingCount: 1}})
-    const following = await this.userModel.findByIdAndUpdate(followingId, { $inc: {followerCount: 1}})
-    
+    const follower = await this.userModel.findByIdAndUpdate(followerId, {
+      $inc: { followingCount: 1 },
+    });
+    const following = await this.userModel.findByIdAndUpdate(followingId, {
+      $inc: { followerCount: 1 },
+    });
+
     const notificationData: sendNotification = {
       expoPushToken: following.fcmToken,
       sender: follower,
       reciever: following,
-    }
+    };
 
     await this.notificationService.sendFollowNotification(notificationData);
-    return following
+    following.isFollowing = true;
+    return following;
   }
 
-  async unFollow(followerId: MongooSchema.Types.ObjectId, followingId: MongooSchema.Types.ObjectId) {
+  async unFollow(
+    followerId: MongooSchema.Types.ObjectId,
+    followingId: MongooSchema.Types.ObjectId,
+  ) {
     if (followerId === followingId) {
       throw new Error("you_can't_unfollow_yourself");
     }
@@ -74,42 +87,45 @@ export class FolloweService {
       $inc: { followerCount: -1 },
     });
 
+    following.isFollowing = false;
     return following;
   }
 
   async getFollowings(userId: MongooSchema.Types.ObjectId) {
     const followings = await this.followerModel
-      .find({ follower: userId, deleted_at: { $eq: null } }) 
-      .populate('following') 
+      .find({ follower: userId, deleted_at: { $eq: null } })
+      .populate('following')
       .exec();
-    
-    const result = followings.map(f => f.following); 
-    const followingIds = new Set(followings.map(f => f.following.id.toString()));
+
+    const result = followings.map((f) => f.following);
+    const followingIds = new Set(
+      followings.map((f) => f.following.id.toString()),
+    );
 
     result.forEach((user: any) => {
       user.isFollowing = followingIds.has(user._id.toString());
     });
-    
+
     return result;
   }
 
   async getFollowers(userId: MongooSchema.Types.ObjectId) {
     const followers = await this.followerModel
-      .find({ following: userId, deleted_at: { $eq: null } }) 
+      .find({ following: userId, deleted_at: { $eq: null } })
       .populate('follower')
       .exec();
-    
-    const result = followers.map(f => f.follower); 
-    
-    const followDocs = await this.followerModel.find({
-      follower: userId,
-      following: { $in: result.map(u => u.id) }
-    })
-    .select('following');
-    
-    const followingIds = new Set(followDocs.map(f => f.following.toString()));
 
-    
+    const result = followers.map((f) => f.follower);
+
+    const followDocs = await this.followerModel
+      .find({
+        follower: userId,
+        following: { $in: result.map((u) => u.id) },
+      })
+      .select('following');
+
+    const followingIds = new Set(followDocs.map((f) => f.following.toString()));
+
     result.forEach((user: any) => {
       user.isFollowing = followingIds.has(user._id.toString());
     });
